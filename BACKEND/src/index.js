1a0d50f4e9ec34d6cb075376d432ee460047eb8d
Subrata Biswas <subratabiswasofficial@gmail.com>
2020-07-14 16:26:52 +0530

    Update index.js

--- BACKEND/src/index.js
+++ BACKEND/src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const bcrypt = require("bcrypt");
 
 require("dotenv").config();
 require("./database/mongoose");
